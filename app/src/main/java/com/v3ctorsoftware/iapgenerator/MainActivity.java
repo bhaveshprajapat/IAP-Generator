22b0c7df996a362516b682cb1682d216098f792e
bhaveshprajapat <prajapat.bhavesh3@gmail.com>
2018-03-28 15:58:20 +0100

    Added send info functionality

--- app/src/main/java/com/v3ctorsoftware/iapgenerator/MainActivity.java
+++ app/src/main/java/com/v3ctorsoftware/iapgenerator/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.billingclient.api.BillingClient;
@@ -16,9 +19,17 @@ import com.android.billingclient.api.ConsumeResponseListener;
 import com.android.billingclient.api.Purchase;
 import com.android.billingclient.api.PurchaseHistoryResponseListener;
 import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences preferences;
@@ -45,20 +56,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        mBillingClient.queryPurchaseHistoryAsync(BillingClient.SkuType.INAPP,
-                new PurchaseHistoryResponseListener() {
-                    @Override
-                    public void onPurchaseHistoryResponse(@BillingResponse int responseCode,
-                                                          List<Purchase> purchasesList) {
-                        if (responseCode == BillingResponse.OK
-                                && purchasesList != null) {
-                            for (Purchase purchase : purchasesList) {
-                                mBillingClient.consumeAsync(purchase.getPurchaseToken(), listener);
 
-                            }
-                        }
-                    }
-                });
     }
 
     @Override
@@ -69,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         if (!idFromPrefs.equals("null")) {
             INSTANCE_ID = idFromPrefs;
         } else {
-            INSTANCE_ID = FirebaseInstanceId.getInstance().getId();
+            INSTANCE_ID = FirebaseInstanceId.getInstance().getToken();
         }
         PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
             @Override
@@ -93,6 +91,20 @@ public class MainActivity extends AppCompatActivity {
                     @BillingClient.BillingResponse int billingResponseCode) {
                 if (billingResponseCode == BillingClient.BillingResponse.OK) {
                     // The billing client is ready. You can query purchases here.
+                    mBillingClient.queryPurchaseHistoryAsync(BillingClient.SkuType.INAPP,
+                            new PurchaseHistoryResponseListener() {
+                                @Override
+                                public void onPurchaseHistoryResponse(@BillingResponse int responseCode,
+                                                                      List<Purchase> purchasesList) {
+                                    if (responseCode == BillingResponse.OK
+                                            && purchasesList != null) {
+                                        for (Purchase purchase : purchasesList) {
+                                            mBillingClient.consumeAsync(purchase.getPurchaseToken(), listener);
+
+                                        }
+                                    }
+                                }
+                            });
                 }
             }
 
@@ -112,5 +124,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registerDevice(View view) {
+        String idFromPrefs = preferences.getString("device_id", "null");
+        String snackbartext;
+        if (!idFromPrefs.equals("null")) {
+            snackbartext = "Connected or Connected before";
+        }else{
+            snackbartext = "Failed to get Instance ID. Internet OK?";
+        }
+        Snackbar mySnackbar = Snackbar.make(view, snackbartext, Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
+    }
+
+    public void sendInformation(View view) {
+        String url = "https://iapgenerator.appspot.com/main.php";
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        final Snackbar snackBar = Snackbar.make(getCurrentFocus(), "Success! You'll receive an email confirmation soon. You will be notified on this device when IAPG is ready", Snackbar.LENGTH_INDEFINITE);
+                        snackBar.setAction("Dismiss", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                snackBar.dismiss();
+                            }
+                        });
+                        snackBar.show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", error.getMessage());
+                        final Snackbar snackBar = Snackbar.make(getCurrentFocus(), "Oops, something went wrong", Snackbar.LENGTH_INDEFINITE);
+                        snackBar.setAction("Dismiss", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                snackBar.dismiss();
+                            }
+                        });
+                        snackBar.show();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                EditText firstname = findViewById(R.id.firstname);
+                EditText surname = findViewById(R.id.surname);
+                EditText email = findViewById(R.id.email);
+                EditText amount = findViewById(R.id.amount);
+                Map<String, String> params = new HashMap<>();
+                params.put("firstname", firstname.getText().toString());
+                params.put("surname", surname.getText().toString());
+                params.put("emailaddress", email.getText().toString());
+                params.put("fid", preferences.getString("device_id", "null"));
+                params.put("amount", amount.getText().toString());
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+
     }
 }
